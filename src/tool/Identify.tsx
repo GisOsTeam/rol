e5bf3955255f485f22e42cdae8e268e40d3633ad
dbelzunce <donatien.belzunce@sword-group.com>
2019-12-13 16:03:38 +0100

    useIdentify Hook's way

---
+++ src/tool/Identify.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { useIdentify, IIdentifyResponse } from './useIdentify';
+import { IBaseToolProps, withBaseTool } from './BaseTool';
+export const Identify = withBaseTool((props: IBaseToolProps) : React.ReactElement => {
+    useIdentify({
+        activated: (props.activated ? props.activated : false),
+        callBack: (identifyResp: IIdentifyResponse) => {
+            console.log("identifyResp", identifyResp);
+        }
+    });    
+    return null;
+});
---
+++ src/tool/useIdentify.tsx
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import { Feature, MapBrowserEvent } from 'ol';
+import OlBaseLayer from 'ol/layer/Base';
+import { Pixel } from 'ol/pixel';
+import { rolContext } from '../RolContext';
+import { IQueryResponse, constructQueryRequestFromPixel, IExtended, IQueryFeatureTypeResponse } from '@gisosteam/aol/source';
+
+export interface IIdentifyResponse {
+    features: { [key: string]: Feature[] }
+}
+
+export interface IUseIdentifyProps {
+    activated: boolean;
+    callBack: (identifyResp: IIdentifyResponse) => unknown;
+};
+
+export function useIdentify(props: IUseIdentifyProps): Promise<IIdentifyResponse> {
+    const context = React.useContext(rolContext);
+    const { olMap } = context;
+
+    React.useEffect(() => {
+        const onClick = (clickEvent: MapBrowserEvent) => {
+            identify(clickEvent.pixel);
+        };
+                
+        const identify = (pixel: Pixel) => {
+            if (olMap && pixel) {
+                const promises: Array<Promise<IQueryResponse>> = [];
+                const queryRequest = constructQueryRequestFromPixel(pixel, 2, olMap);
+                queryRequest.limit = 10;
+                olMap.forEachLayerAtPixel(pixel, (layer: OlBaseLayer) => {
+                    if (layer.getVisible() && 'getSource' in layer) {
+                        const source = (layer as any).getSource();
+                        if (source && 'query' in source) {
+                            promises.push((source as IExtended).query(queryRequest));
+                        }
+                    }
+                });
+                return Promise.all(promises).then((queryResponses: IQueryResponse[]) => {
+                    const features: any = {};
+                    queryResponses.forEach((queryResponse: IQueryResponse) => {
+                        const ftResps = queryResponse.featureTypeResponses;
+                        ftResps.forEach((ftResp: IQueryFeatureTypeResponse) => {
+                            const type = ftResp.type ? ftResp.type.id : 'unknown';
+                            if (!features[type]) {
+                                features[type] = [];
+                            }
+                            features[type].push(...ftResp.features);
+                        });
+                    });
+                    props.callBack({ features });
+                });
+            }
+        }
+
+        if (props.activated) {
+            olMap.on('click', onClick);
+        } else {
+            olMap.un('click', onClick)
+        }
+        return () => {
+            olMap.un('click', onClick);    
+        };
+    }, [props.activated, props.callBack]);
+
+    
+    return null;
+}

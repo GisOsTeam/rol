f9f03e9fde16abb587e59a2578de410ae5908ea6
dbelzunce <donatien.belzunce@sword-group.com>
2019-12-17 15:49:21 +0100

    Fix FeatureTable on Draw

--- src/tool/featureTable/FeatureTable.tsx
+++ src/tool/featureTable/FeatureTable.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { IIdentifyResponse } from '../hook/useIdentify';
-import { Feature } from 'ol';
+import { Feature, getUid } from 'ol';
 
 // CSS
 import '../../assets/css/featureTable.css';
@@ -26,7 +26,8 @@ export const FeatureTable : React.FC<IFeatureTableProps> = ({ features } : IFeat
         const htmlEntities: React.ReactElement[] = displayedObjects.map((feature: Feature, featureIndex: number) => {
             const customFeatureId = 'feature_id';
             let customFeat: any = {};
-            customFeat[customFeatureId] = feature.getId().toString();
+            const id = (feature.getId && feature.getId()) ? feature.getId() : getUid(feature);
+            customFeat[customFeatureId] = id;
             customFeat = {
                 ...customFeat,
                 ...feature.getProperties()
@@ -50,12 +51,13 @@ export const FeatureTable : React.FC<IFeatureTableProps> = ({ features } : IFeat
             featureSummary[key] = []
         }
         features[key].forEach((feature) => {
-            featureSummary[key].push(feature.getId().toString());
+            const id = (feature.getId && feature.getId()) ? feature.getId() : getUid(feature);
+            featureSummary[key].push(id.toString());
         });
     });
 
     const onClickTab = (key: string, value: string) => {
-        setDisplayedObjects(features[key].filter((feat) => feat.getId() === value));
+        setDisplayedObjects(features[key].filter((feat) => feat.getId() === value || getUid(feat) === value));
     };
 
     return (<div className="features-by-layers-wrapper">

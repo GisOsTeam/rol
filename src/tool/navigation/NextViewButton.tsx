74e22191d9a015c238e4801bc71a45067d5dc1e3
dbelzunce <donatien.belzunce@sword-group.com>
2020-04-24 11:14:27 +0200

    init css

--- src/tool/navigation/NextViewButton.tsx
+++ src/tool/navigation/NextViewButton.tsx
@@ -2,20 +2,29 @@ import * as React from 'react';
 import styled from 'styled-components';
 import { IBaseButtonToolProps, withBaseButtonTool } from '../BaseButtonTool';
 import { useViewManager } from '../hook/useViewManager';
+import { useTranslate } from '../hook/useTranslate';
 
 const ContainerBtn = styled.div`
   height: 28px;
+  width: 32px;
+  height: 28px;
+  &:after {
+    content: '⤻';
+    font-weight: bold;
+    vertical-align: middle;
+  }
 `;
 
 export const NextViewButton = withBaseButtonTool(
   (props: IBaseButtonToolProps) => {
     const { fitToNext } = useViewManager();
+    const translate = useTranslate();
     React.useEffect(() => {
       if (props.activated === true) {
         fitToNext();
       }
     }, [props.activated]);
-    return <ContainerBtn>Next</ContainerBtn>;
+    return <ContainerBtn title={translate('navigation.next', 'Next View')} />;
   },
   { className: 'counter-button', toggle: false }
 );
--- src/tool/navigation/PreviousViewButton.tsx
+++ src/tool/navigation/PreviousViewButton.tsx
@@ -2,20 +2,28 @@ import * as React from 'react';
 import styled from 'styled-components';
 import { IBaseButtonToolProps, withBaseButtonTool } from '../BaseButtonTool';
 import { useViewManager } from '../hook/useViewManager';
+import { useTranslate } from '../hook/useTranslate';
 
 const ContainerBtn = styled.div`
+  width: 32px;
   height: 28px;
+  &:after {
+    content: '⤺';
+    font-weight: bold;
+    vertical-align: middle;
+  }
 `;
 
 export const PreviousViewButton = withBaseButtonTool(
   (props: IBaseButtonToolProps) => {
     const { fitToPrevious } = useViewManager();
+    const translate = useTranslate();
     React.useEffect(() => {
       if (props.activated === true) {
         fitToPrevious();
       }
     }, [props.activated]);
-    return <ContainerBtn>Previous</ContainerBtn>;
+    return <ContainerBtn title={translate('navigation.previous', 'Previous View')} />;
   },
   { className: 'counter-button', toggle: false }
 );

1aa3ab32a64eefaaf416d2db1e1cc9cba1ddd812
BELZUNCE Donatien <donatien.belzunce@sword-group.com>
2020-07-13 09:21:57 +0200

    lint

--- src/sample/SampleApp.tsx
+++ src/sample/SampleApp.tsx
@@ -35,41 +35,41 @@ const wkt2154 =
 const wkt27700 =
   'PROJCS["OSGB 1936 / British National Grid",GEOGCS["OSGB 1936",DATUM["OSGB_1936",SPHEROID["Airy 1830",6377563.396,299.3249646,AUTHORITY["EPSG","7001"]],AUTHORITY["EPSG","6277"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.01745329251994328,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4277"]],UNIT["metre",1,AUTHORITY["EPSG","9001"]],PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",49],PARAMETER["central_meridian",-2],PARAMETER["scale_factor",0.9996012717],PARAMETER["false_easting",400000],PARAMETER["false_northing",-100000],AUTHORITY["EPSG","27700"],AXIS["Easting",EAST],AXIS["Northing",NORTH]]';
 
-  const osm = new Xyz({
-    url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    projection: 'EPSG:3857'
-  });
-  
-  const world2D = new TileArcGISRest({
-    url: 'https://services.arcgisonline.com/arcgis/rest/services/ESRI_Imagery_World_2D/MapServer',
-    // url: 'http://localhost:8181/aHR0cHM6Ly9zZXJ2aWNlcy5hcmNnaXNvbmxpbmUuY29tL2FyY2dpcy9yZXN0L3NlcnZpY2VzL0VTUklfSW1hZ2VyeV9Xb3JsZF8yRA%3D%3D/MapServer',
-    projection: 'EPSG:3857'
-  });
-  
-  const britishNationalGrid = new ImageStatic({
-    url:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/British_National_Grid.svg/2000px-British_National_Grid.svg.png',
-    projection: 'EPSG:27700',
-    imageExtent: [0, 0, 700000, 1300000]
-  });
-  
-  const toppStateSource = new TileWms({
-    url: 'https://ahocevar.com/geoserver/wms',
-    types: [{ id: 'topp:states' }],
-    params: {}
-  });
-  
-  const cities = new ImageWms({
-    url: 'https://demo.mapserver.org/cgi-bin/wms',
-    types: [{ id: 'cities' }],
-    params: {}
-  });
-  
-  const highways = new ImageArcGISRest({
-    url: 'https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer',
-    types: [{ id: 2 }],
-    params: {}
-  });
+const osm = new Xyz({
+  url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  projection: 'EPSG:3857',
+});
+
+const world2D = new TileArcGISRest({
+  url: 'https://services.arcgisonline.com/arcgis/rest/services/ESRI_Imagery_World_2D/MapServer',
+  // url: 'http://localhost:8181/aHR0cHM6Ly9zZXJ2aWNlcy5hcmNnaXNvbmxpbmUuY29tL2FyY2dpcy9yZXN0L3NlcnZpY2VzL0VTUklfSW1hZ2VyeV9Xb3JsZF8yRA%3D%3D/MapServer',
+  projection: 'EPSG:3857',
+});
+
+const britishNationalGrid = new ImageStatic({
+  url:
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/British_National_Grid.svg/2000px-British_National_Grid.svg.png',
+  projection: 'EPSG:27700',
+  imageExtent: [0, 0, 700000, 1300000],
+});
+
+const toppStateSource = new TileWms({
+  url: 'https://ahocevar.com/geoserver/wms',
+  types: [{ id: 'topp:states' }],
+  params: {},
+});
+
+const cities = new ImageWms({
+  url: 'https://demo.mapserver.org/cgi-bin/wms',
+  types: [{ id: 'cities' }],
+  params: {},
+});
+
+const highways = new ImageArcGISRest({
+  url: 'https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer',
+  types: [{ id: 2 }],
+  params: {},
+});
 
 export class SampleApp extends React.Component<{}, {}> {
   constructor(props: {}) {
--- src/sample/main.ts
+++ src/sample/main.ts
@@ -7,58 +7,58 @@ import { IRequest, IResponse } from 'bhreq';
 const ReactElement = React.createElement(SampleApp);
 const httpEngine = HttpEngine.getInstance();
 
-document.cookie = "JSESSIONID=5nnxhe3hyvu2";
+document.cookie = 'JSESSIONID=5nnxhe3hyvu2';
 // console.log(parent.document.cookie)
 const oldSend = HttpEngine.prototype.send;
 HttpEngine.prototype.send = async (rawRequest: IRequest) => {
-    const resp: IResponse = {
-        body: {
-            val: 'test'
-        },
-        contentType: 'JSON',
-        headers: {},
-        responseType: 'json',
-        status: 200,
-        statusText: 'OK',
-        text: 'Ook'
-    };
-    const requestInit: RequestInit = {
-        ...rawRequest,
-        headers: { ...rawRequest.headers, 
-            "Cookie": "JSESSIONID=5nnxhe3hyvu2",
-        },
-        credentials: 'include' 
-    }
-    const fetchResp = await fetch(rawRequest.url, requestInit)
-    const jsonResp = await fetchResp.json();
-    const headers: { [key: string] : string } = {};
-    fetchResp.headers.forEach((value: any, key: string) => { headers[key] = value });
-    
-    console.log(headers)
-    const response: IResponse = {
-        body: jsonResp,
-        text: JSON.stringify(jsonResp),
-        statusText: fetchResp.statusText,
-        status: fetchResp.status,
-        contentType: fetchResp.headers.get("content-type"),
-        responseType: 'json',
-        headers 
-    };
-    return response;
+  const resp: IResponse = {
+    body: {
+      val: 'test',
+    },
+    contentType: 'JSON',
+    headers: {},
+    responseType: 'json',
+    status: 200,
+    statusText: 'OK',
+    text: 'Ook',
+  };
+  const requestInit: RequestInit = {
+    ...rawRequest,
+    headers: { ...rawRequest.headers, Cookie: 'JSESSIONID=5nnxhe3hyvu2' },
+    credentials: 'include',
+  };
+  const fetchResp = await fetch(rawRequest.url, requestInit);
+  const jsonResp = await fetchResp.json();
+  const headers: { [key: string]: string } = {};
+  fetchResp.headers.forEach((value: any, key: string) => {
+    headers[key] = value;
+  });
+
+  console.log(headers);
+  const response: IResponse = {
+    body: jsonResp,
+    text: JSON.stringify(jsonResp),
+    statusText: fetchResp.statusText,
+    status: fetchResp.status,
+    contentType: fetchResp.headers.get('content-type'),
+    responseType: 'json',
+    headers,
+  };
+  return response;
 };
 
 const sampleBeforeSendInterceptor: BeforeSendInterceptor = (request: IRequest) => {
-    const jsession = document.cookie.split(';').find((cook) => cook.match('JSESSIONID=')) || null;
-    const alteredReq: IRequest = {
-        ...request,
-        headers: {
-            ...request.headers,
-            "Cookies": jsession
-        }
-    }
-    console.log("Intercept", request, alteredReq)
-    return request;
-}
+  const jsession = document.cookie.split(';').find((cook) => cook.match('JSESSIONID=')) || null;
+  const alteredReq: IRequest = {
+    ...request,
+    headers: {
+      ...request.headers,
+      Cookies: jsession,
+    },
+  };
+  console.log('Intercept', request, alteredReq);
+  return request;
+};
 
 httpEngine.beforeSendInterceptors.push(sampleBeforeSendInterceptor);
 ReactDOM.render(ReactElement, document.getElementById('root'));

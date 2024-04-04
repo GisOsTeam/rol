a82ced5163944b6db96fb7953ec18c83b65215d3
Mathieu Mast <mathieu.mast>
2024-04-04 15:10:12 +0200

    Upg deps

--- src/sample/main.ts
+++ src/sample/main.ts
@@ -3,4 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { SampleApp } from './SampleApp';
 
 const container = document.getElementById('root');
-createRoot(container!).render(React.createElement(SampleApp));
+const root = createRoot(container!);
+const element = React.createElement(SampleApp);
+root.render(element as any);

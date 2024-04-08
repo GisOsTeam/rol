147ad6215a7e9e5f75e5eeb49e34d03ef59e1e55
Mathieu Mast <mathieu.mast>
2024-04-08 09:24:19 +0200

    Add WebGLTile

--- src/sample/SampleApp.tsx
+++ src/sample/SampleApp.tsx
@@ -64,9 +64,11 @@ const highways = new ImageArcGISRest({
 });
 
 const tci = new GeoTIFF({
-  sources: [{
-    url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/36/Q/WD/2020/7/S2A_36QWD_20200701_0_L2A/TCI.tif',
-  }],
+  sources: [
+    {
+      url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/36/Q/WD/2020/7/S2A_36QWD_20200701_0_L2A/TCI.tif',
+    },
+  ],
 });
 
 export class SampleApp extends React.Component<never, { hide: boolean }> {
